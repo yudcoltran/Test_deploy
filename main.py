09c40ec2f845ef38b88a5ba48a6c7c47e3dcd6b9
loctd <v.loctd2@vinbigdata.com>
2023-07-27 15:55:19 +0700

    .

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ class User(BaseModel):
 def ping(body: User):
     pass
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8000)
